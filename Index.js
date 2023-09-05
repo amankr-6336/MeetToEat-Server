1caeed96f5689ce7653a8706dac1ee9b5caf8d98
amankr.6336 <amankr.17709418@gmail.com>
2023-09-05 13:18:34 +0530

    cloud

--- Index.js
+++ Index.js
@@ -11,9 +11,9 @@ dotenv.config('./env')
 
 
 cloudinary.config({ 
-    cloud_name: `${process.env.CLOUD_NAME}`, 
-    api_key: `${process.env.API_KEY}`, 
-    api_secret: `${process.env.API_SECRET}` 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET 
   });
   
 
